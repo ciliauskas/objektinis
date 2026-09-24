4ee6cfe9028fbc6908a9abec8eb0e7560f405f7f
Fausta Vaituleviciute <vaituleviciute.v@gmail.com>
2026-09-24 13:16:50 +0300

    prideti galutionio balo, vidurkio ir medianos skaiciavimai

--- main.cpp
+++ main.cpp
@@ -18,6 +18,25 @@ struct Studentas {
     double gmediana;
 };
 
+double svidurkis(vector<int> v) {
+    if (v.size() == 0) return 0.0;
+    double suma = 0.0;
+    for (int i = 0; i < v.size(); i++) suma += v[i];
+    return suma / v.size();
+}
+double smediana(vector<int> v) {
+    if (v.size() == 0) return 0.0;
+    sort(v.begin(), v.end());
+    int n = v.size();
+    if (n % 2 != 0) return (double)(v[n / 2]);
+    else return (double)(v[n / 2 - 1] + v[n / 2]) / 2.0;
+}
+
+void galutinis(Studentas &s) {
+    s.gvidurkis = 0.4 * svidurkis(s.nd) + 0.6 * s.egzaminas;
+    s.gmediana = 0.4 * smediana(s.nd) + 0.6 * s.egzaminas;
+}
+
 int main() {
     return 0;
 }
