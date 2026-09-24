8168f111e7b94f3814d72f74e11f4c59df271e5e
Fausta Vaituleviciute <faustavaituleviciute@Mac.lan>
2026-09-24 12:56:14 +0300

    sukurta bazine studento struktura

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+struct Studentas {
+    string vardas;
+    string pavarde;
+    vector<int> nd;
+    int egzaminas;
+    double gvidurkis;
+    double gmediana;
+};
+
+int main() {
+    return 0;
+}
